3c0ea4c1c34b467df6d05bc87aa76c0b9e7e874b
swindletheconman <swindletheconman@gmail.com>
2025-01-11 17:43:18 +1100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Hello This is my Brand New Website")
+st.header("Written with 100% Python code")
+st.write("click the button for fun surprise")
+
+if st.button("Snowflake"):
+    st.snow()
+    
+if st.button("Balloon"):
+    st.button()
+    
+st.write("I'll put something great here so stay tunned")
