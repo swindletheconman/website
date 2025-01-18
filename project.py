0724b278aabe5e7db2bfaf07a40ba132ab481711
swindletheconman <swindletheconman@gmail.com>
2025-01-18 14:13:45 +1100

    Update project.py
    sidebar added

--- project.py
+++ project.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with st.sidebar:
+    st.write("Choose Web App")
+
 st.title("Hello This is my Brand New Website")
 st.header("Written with 100% Python code")
 st.write("click the button for fun surprise")
