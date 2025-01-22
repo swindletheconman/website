d1bab9ef6222d724b77e4da22763e30bc4916fb1
swindletheconman <swindletheconman@gmail.com>
2025-01-22 11:26:39 +1100

    Update market.py

--- market.py
+++ market.py
@@ -5,6 +5,10 @@ data = pd.read_excel('./pages/source.xlsx)
 
 st.dataframe(data)
 
+#develop search criteria
 criteria1 = data['category'] == 'non food'
+criteria2 = data['store_name'] == 'indomaret'
 
+#to apply the criteria
 st.dataframe(data[criteria1])
+st.dataframe(data[criteria2])
