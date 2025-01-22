bd424f59bc0aefac21a8f6d38c7431af70debc1b
swindletheconman <swindletheconman@gmail.com>
2025-01-22 11:20:37 +1100

    Update market.py

--- market.py
+++ market.py
@@ -1,2 +1,10 @@
 import streamlit as st
 import pandas as pd
+
+data = pd.read_excel('./pages/source.xlsx)
+
+st.dataframe(data)
+
+criteria1 = data['category'] == 'non food'
+
+st.dataframe(data[criteria1])
