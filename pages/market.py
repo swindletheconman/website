68ac77c62acc41d78de449c112b3d7026726a792
swindletheconman <swindletheconman@gmail.com>
2025-01-22 11:52:03 +1100

    Update market.py

--- pages/market.py
+++ pages/market.py
@@ -3,6 +3,20 @@ import pandas as pd
 
 data = pd.read_excel('./pages/source.xlsx')
 
-st.dataframe(data)
+#st.dataframe(data)
 
-criteria1 = data['category']
+#develop search criteria
+criteria1 = data['category'] == 'non food'
+criteria2 = data['store_name'] == 'Indomaret'
+criteria3 = (criteria1) & (criteria2)
+criteria4 = data['price'] >= 10000
+criteria5 = data['price'] <= 30000
+criteria6 = (criteria4) & (criteria5)
+
+#to apply the criteria
+st.dataframe(data[criteria1])
+st.dataframe(data[criteria2])
+st.dataframe(data[criteria3])
+st.dataframe(data[criteria4])
+st.dataframe(data[criteria5])
+st.dataframe(data[criteria6])
