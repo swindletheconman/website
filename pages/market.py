eb1db0ba1555462f2c9e47d42980a542ee6d2cb5
swindletheconman <swindletheconman@gmail.com>
2025-02-01 17:57:15 +1100

    Update market.py

--- pages/market.py
+++ pages/market.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 data = pd.read_excel('./pages/source.xlsx')
-st.dataframe(data)
+#st.dataframe(data)
 
 category_values = data['category'].unique()
 selected_category = st.multiselect("Seclect Category", options=category_values)
@@ -17,16 +17,16 @@ price_range = st.slider("Price range",min_value=minimum_price,max_value=maximum_
 
 #develop search criteria
 criteria1 = data['category'].isin(selected_category)
-criteria2 = data['store_name'].isin(selected_category)
+criteria2 = data['store_name'].isin(selected_store)
 criteria3 = (criteria1) & (criteria2)
 criteria4 = data['price'] >= 10000
 criteria5 = data['price'] <= 30000
 criteria6 = (criteria4) & (criteria5)
 
 #to apply the criteria
-st.dataframe(data[criteria1])
-st.dataframe(data[criteria2])
+#st.dataframe(data[criteria1])
+#st.dataframe(data[criteria2])
 st.dataframe(data[criteria3])
-st.dataframe(data[criteria4])
-st.dataframe(data[criteria5])
-st.dataframe(data[criteria6])
+#st.dataframe(data[criteria4])
+#st.dataframe(data[criteria5])
+#st.dataframe(data[criteria6])
