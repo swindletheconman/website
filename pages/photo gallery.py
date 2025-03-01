04afd486f0184dddf39d9faf95b012d30fe5d733
swindletheconman <swindletheconman@gmail.com>
2025-03-01 17:54:46 +1100

    Update photo gallery.py

--- pages/photo gallery.py
+++ pages/photo gallery.py
@@ -4,9 +4,10 @@ import os
 folder = os.listdir('images')
 length_of_images = len(folder)
 
-for i in range(ncolumns):
+for i in range(length_of_images):
   for c in range(ncolumns):
     if i%ncolumns == c:
       col == columns[c]
       with col:
         with st.container(border=True):
+          st.image(r'./images/'+folder[i])
