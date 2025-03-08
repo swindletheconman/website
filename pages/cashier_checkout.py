135255d04816543d2abdc0cc30b342d3a7426cfd
swindletheconman <swindletheconman@gmail.com>
2025-03-08 17:25:30 +1100

    Create cashier_checkout.py

---
+++ pages/cashier_checkout.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+leftcolumn,rightcolumn = st.columns(2)
+
+with leftcolumn:
+  pass
+  with st.container(border=True):
+    pass
+
+with rightcolumn:
+  pass
+  with st.container(border=True):
+    pass
