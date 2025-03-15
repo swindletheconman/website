ebf0e3af9ca6911a1a364e46ea2838bc9da52a57
swindletheconman <swindletheconman@gmail.com>
2025-03-15 17:24:47 +1100

    Update cashier_checkout.py

--- pages/cashier_checkout.py
+++ pages/cashier_checkout.py
@@ -29,8 +29,10 @@ with leftcolumn:
 
   with st.container(border=True):
     st.write("Return:")
+    return_amount = payment_received - total_amount
+    st.write(f'{return_amount}')
 
 with rightcolumn:
   pass
   with st.container(border=True):
-    pass
+    st.write('right')
