2b7358012a97f39f0af3bc224aa81129a5396fd9
swindletheconman <swindletheconman@gmail.com>
2025-03-29 17:59:17 +1100

    Update project.py

--- project.py
+++ project.py
@@ -18,14 +18,13 @@ st.write("I'll put something great here so stay tunned")
 pages = {
     "Prodjects" : [
         st.Page("./pages/Convsion.py", title = "Converter"),
-        st.Page("./pages/Market.py", title - "Online Store"),
-        st.Page("./pages/Cashier_checkout.py", title - "Cashier"),
+        st.Page("./pages/Market.py", title = "Online Store"),
+        st.Page("./pages/Cashier_checkout.py", title = "Cashier"),
         st.Page("./pages/04_Triangle.py", title = "Triangle")
     ],
     "Tools" : [
         st.Page("./pages/photo gallery.py", title = "Rotate PDF"),
         st.Page("./pages/newapp.py", title = "Inventory Simulation"),
-
     ],
 }
 
