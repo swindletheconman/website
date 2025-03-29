c21f4c06ab3d187cc9b2312fca730f8aa2cbf75a
swindletheconman <swindletheconman@gmail.com>
2025-03-29 17:57:13 +1100

    Update project.py

--- project.py
+++ project.py
@@ -14,3 +14,20 @@ if st.button("Balloon"):
     st.balloons()
     
 st.write("I'll put something great here so stay tunned")
+
+pages = {
+    "Prodjects" : [
+        st.Page("./pages/Convsion.py", title = "Converter"),
+        st.Page("./pages/Market.py", title - "Online Store"),
+        st.Page("./pages/Cashier_checkout.py", title - "Cashier"),
+        st.Page("./pages/04_Triangle.py", title = "Triangle")
+    ],
+    "Tools" : [
+        st.Page("./pages/photo gallery.py", title = "Rotate PDF"),
+        st.Page("./pages/newapp.py", title = "Inventory Simulation"),
+
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
