619ff4653cbaa1b89465ec0699c4493ef7c1772c
swindletheconman <swindletheconman@gmail.com>
2025-04-12 17:12:21 +1000

    Update cashier_checkout.py

--- pages/cashier_checkout.py
+++ pages/cashier_checkout.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-st.set_page_config(layout='wide')
-
 dummy_data = {'description':['blueband','bread','egg'],
               'price':[12000,18000,15000],
               'quantity':[1,2,3]}
